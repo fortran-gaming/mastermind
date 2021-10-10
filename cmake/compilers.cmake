edaae2258c47eae83eb175dcb224fd43579745e3
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-10 11:27:48 -0400

    cmake best practices compiler flags

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,14 +1,12 @@
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-mtune=native -Wall -Wextra -Werror=array-bounds -fimplicit-none)
-
-  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -fcheck=all -fexceptions -ffpe-trap=invalid,zero,overflow -finit-real=nan -Wconversion")
+  add_compile_options(-mtune=native -Wall -Wextra -Werror=array-bounds -fimplicit-none
+  $<$<CONFIG:Release>:-fno-backtrace>
+  "$<$<CONFIG:Debug>:-fcheck=all;-fexceptions;-ffpe-trap=invalid,zero,overflow;-finit-real=nan;-Wconversion>"
+  )
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
-  if(WIN32)
-    add_compile_options(/warn)
-  else()
-    add_compile_options(-warn)
-    string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -fpe0 -traceback -debug extended -check all")
-  endif()
+  add_compile_options(-warn
+  "$<$<CONFIG:Debug>:-fpe0;-traceback;-debug extended;-check all>"
+  )
 endif()
