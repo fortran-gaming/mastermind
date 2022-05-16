4d67d9a76ebd44becc4febfa4945a014291083d2
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-16 11:54:32 -0400

    flag-lint

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,12 +1,11 @@
-set(CMAKE_EXPORT_COMPILE_COMMANDS on)
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  add_compile_options(-mtune=native -Wall -Wextra -Werror=array-bounds -fimplicit-none
+  add_compile_options(-Wall -Wextra -Werror=array-bounds -fimplicit-none
   $<$<CONFIG:Release>:-fno-backtrace>
   "$<$<CONFIG:Debug>:-fcheck=all;-fexceptions;-ffpe-trap=invalid,zero,overflow;-finit-real=nan;-Wconversion>"
   )
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
-  add_compile_options(-warn
-  "$<$<CONFIG:Debug>:-fpe0;-traceback;-debug extended;-check all>"
+  add_compile_options(-warn -traceback
+  "$<$<CONFIG:Debug>:-fpe0;-debug;-check>"
   )
 endif()
